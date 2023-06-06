efb67d36e9c7c21fc933a4f977e331abac4227de
PMani123 <manikandan.p@cloudangles.com>
2023-06-06 11:18:06 +0530

    06-06-23

---
+++ _4_feature_engineering.py
@@ -0,0 +1,18 @@
+from _3_data_preprocessing import data_preprocessing
+from sklearn import preprocessing
+
+label_encoder = preprocessing.LabelEncoder()
+
+def feature_engineering():
+    df = data_preprocessing()
+    df['POSTED_BY'] = label_encoder.fit_transform(df['POSTED_BY'])
+    df['BHK_OR_RK'] = label_encoder.fit_transform(df['BHK_OR_RK'])
+
+    df = df.drop(columns = ["READY_TO_MOVE", "ADDRESS"], axis=1)
+    
+    df.to_csv('cleaned.csv')
+    print("Hello")
+    print(df.shape)
+    return df
+
+feature_engineering()
